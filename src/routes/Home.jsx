2ff4e41ac7034054c38ae0f9e138657997cdfe18
kavispires <kavispires@gmail.com>
2021-12-10 17:08:10 -0800

    Removed unused variables

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, Space } from 'antd';
+import { Space } from 'antd';
 import logo from '../logo.svg';
 import { DoubleRightOutlined } from '@ant-design/icons';
-import { useHistory } from 'react-router-dom';
 
 function Home() {
-  const history = useHistory();
   const [count, setCount] = useState(0);
   const [showMenu, setShowMenu] = useState(false);
 
   const increaseCount = () => {
     setCount(count + 1);
-  };
-
-  const handleClick = ({ key }) => {
-    history.push(`/${key}`);
   };
 
   useEffect(() => {
@@ -44,14 +38,6 @@ function Home() {
             <DoubleRightOutlined /> Arte Ruim: Level 4
           </Link>
         </Space>
-        // <Menu mode="horizontal" onClick={handleClick}>
-        //   <Menu.Item key="parser" icon={<DoubleRightOutlined />}>
-        //     Arte Ruim: Parser
-        //   </Menu.Item>
-        //   <Menu.Item key="level4" icon={<DoubleRightOutlined />}>
-        //     Arte Ruim: Level 4
-        //   </Menu.Item>
-        // </Menu>
       )}
     </div>
   );
