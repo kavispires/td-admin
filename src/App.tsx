0190510b78d0e4d83b482101e25018822592dfcd
kavispires <kavispires@gmail.com>
2022-07-23 11:15:14 -0700

    Added custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import { Home } from './pages/Home';
-import { ArteRuimParser } from './pages/ArteRuimParser';
-import { ArteRuimGroups } from './pages/ArteRuimGroups';
-import { Other } from './pages/Other';
-import { Resource } from './pages/Resource';
+import { Home } from 'pages/Home';
+import { ArteRuimParser } from 'pages/ArteRuimParser';
+import { ArteRuimGroups } from 'pages/ArteRuimGroups';
+import { Other } from 'pages/Other';
+import { Resource } from 'pages/Resource';
+import { ConfigProvider } from 'antd';
+
+ConfigProvider.config({
+  theme: {
+    primaryColor: '#748599',
+    errorColor: '#F8C1D4',
+    warningColor: '#F3D6BC',
+    successColor: '#7FB28A',
+    infoColor: '#2a9d8f',
+  },
+});
 
 function App() {
   return (
