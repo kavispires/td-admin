a9d6eb056e201a0c9c3172974601207c0fc1b086
kavispires <kavispires@gmail.com>
2023-07-21 00:05:31 -0700

    Fixed ArteRuimGroups

--- src/pages/ArteRuimGroups.tsx
+++ src/pages/ArteRuimGroups.tsx
@@ -1,6 +1,5 @@
 import { Input, Layout, List, Typography } from 'antd';
 import { useEffect, useState } from 'react';
-import { useAsync, useTitle } from 'react-use';
 
 import { DataLoadingWrapper } from '../components/DataLoadingWrapper';
 import { ResourceSelectionFilters } from '../components/Resource/ResourceSelectionFilters';
@@ -8,8 +7,11 @@ import { useResourceState } from '../hooks/useResourceState';
 import { LOCALHOST_RESOURCE_URL, RESOURCE_NAMES } from '../utils/constants';
 import { ResourceResponseState } from 'components/Resource/ResourceResponseState';
 import { Header } from 'components/Layout/Header';
+import { useQueryParams } from 'hooks/useQueryParams';
+import { useQuery } from 'react-query';
+import { SectionTitle } from 'components/Common/SectionTitle';
 
-const { Text, Title } = Typography;
+const { Text } = Typography;
 
 const parseData = (cards: Record<CardId, ArteRuimCard>, groups: Record<string, ArteRuimGroup>) => {
   const themes = Object.values(groups)
@@ -22,6 +24,7 @@ const parseData = (cards: Record<CardId, ArteRuimCard>, groups: Record<string, A
   // Get used and unused
   Object.values(groups).forEach((group) => {
     Object.keys(group.cards).forEach((entryId) => {
+      console.log({ entryId });
       if (used[entryId]) {
         duplicated[entryId] = cards[entryId].text;
       } else {
@@ -40,6 +43,9 @@ const parseData = (cards: Record<CardId, ArteRuimCard>, groups: Record<string, A
 };
 
 export function ArteRuimGroups() {
+  // Set default query params
+  useQueryParams({ resourceName: RESOURCE_NAMES.ARTE_RUIM_CARDS, language: 'pt' });
+
   const [used, setUsed] = useState({});
   const [unused, setUnused] = useState({});
   const [duplicated, setDuplicated] = useState({});
@@ -54,17 +60,24 @@ export function ArteRuimGroups() {
   } = useResourceState([RESOURCE_NAMES.ARTE_RUIM_CARDS]);
 
   const {
-    value: groups,
-    loading: loadingLevel4,
+    data: groups,
+    isLoading: loadingLevel4,
     error: errorLevel4,
-  } = useAsync(async () => {
-    const response = await fetch(
-      `${LOCALHOST_RESOURCE_URL}/${RESOURCE_NAMES.ARTE_RUIM_GROUPS}-${language}.json`
-    );
-    const result = await response.json();
+  } = useQuery<any, ResponseError>({
+    queryKey: [RESOURCE_NAMES.ARTE_RUIM_GROUPS, language],
+    queryFn: async () => {
+      const response = await fetch(
+        `${LOCALHOST_RESOURCE_URL}/${RESOURCE_NAMES.ARTE_RUIM_GROUPS}-${language}.json`
+      );
+      const result = await response.json();
 
-    return result;
-  }, [language]);
+      return result;
+    },
+    enabled: Boolean(language),
+  });
+
+  console.log({ cards });
+  console.log({ groups });
 
   useEffect(() => {
     if (!isLoading && !loadingLevel4 && cards && groups) {
@@ -80,66 +93,71 @@ export function ArteRuimGroups() {
     <Layout>
       <Header title="Arte Ruim Groups" subtitle={Boolean(language) ? `${language}` : ''} />
 
-      <ResourceResponseState
-        hasResponseData={hasResponseData && Boolean(groups)}
-        isLoading={isLoading || loadingLevel4}
-        error={error || errorLevel4}
-      />
-      <ResourceSelectionFilters resourceNames={['arte-ruim']} />
+      <Layout hasSider>
+        <Layout.Sider className="sider">
+          <ResourceResponseState
+            hasResponseData={hasResponseData && Boolean(groups)}
+            isLoading={isLoading || loadingLevel4}
+            error={error || errorLevel4}
+          />
+          <ResourceSelectionFilters resourceNames={['arte-ruim']} />
+        </Layout.Sider>
 
-      <Layout.Content className="content">
-        <DataLoadingWrapper
-          isLoading={isLoading || loadingLevel4}
-          error={error || errorLevel4}
-          hasResponseData={hasResponseData && Boolean(groups)}
-        >
-          <div className="parser-container">
-            <div className="parser-main">
-              <Title level={2}>Used in Groups ({Object.keys(used).length})</Title>
-              <Input.TextArea
-                name="output"
-                id=""
-                cols={15}
-                rows={10}
-                readOnly
-                value={JSON.stringify(used, null, 4)}
-              />
-              <Title level={2}>Unused in Groups ({Object.keys(unused).length})</Title>
-              <Input.TextArea
-                name="output"
-                id=""
-                cols={15}
-                rows={10}
-                readOnly
-                value={JSON.stringify(unused, null, 4)}
-              />
-              <Title level={2}>In More than One Group ({Object.keys(duplicated).length})</Title>
-              <Input.TextArea
-                name="duplicates"
-                id=""
-                cols={15}
-                rows={5}
-                readOnly
-                value={JSON.stringify(duplicated, null, 4)}
-              />
+        <Layout.Content className="content">
+          <DataLoadingWrapper
+            isLoading={isLoading || loadingLevel4}
+            error={error || errorLevel4}
+            hasResponseData={hasResponseData && Boolean(groups)}
+          >
+            <div className="parser-container">
+              <div className="parser-main">
+                <SectionTitle>Used in Groups ({Object.keys(used).length})</SectionTitle>
+                <Input.TextArea
+                  name="output"
+                  id=""
+                  cols={15}
+                  rows={10}
+                  readOnly
+                  value={JSON.stringify(used, null, 4)}
+                />
+                <SectionTitle>Unused in Groups ({Object.keys(unused).length})</SectionTitle>
+                <Input.TextArea
+                  name="output"
+                  id=""
+                  cols={15}
+                  rows={10}
+                  readOnly
+                  value={JSON.stringify(unused, null, 4)}
+                />
+                <SectionTitle>In More than One Group ({Object.keys(duplicated).length})</SectionTitle>
+                <Input.TextArea
+                  name="duplicates"
+                  id=""
+                  cols={15}
+                  rows={5}
+                  readOnly
+                  value={JSON.stringify(duplicated, null, 4)}
+                />
+              </div>
+
+              <aside className="parser-controls">
+                <List
+                  header={<SectionTitle>Themes</SectionTitle>}
+                  bordered
+                  className="theme-list"
+                  dataSource={themes}
+                  size="small"
+                  renderItem={(item) => (
+                    <List.Item>
+                      <Text mark></Text> {item}
+                    </List.Item>
+                  )}
+                />
+              </aside>
             </div>
-
-            <aside className="parser-controls">
-              <List
-                header={<Title level={2}>Themes</Title>}
-                bordered
-                dataSource={themes}
-                size="small"
-                renderItem={(item) => (
-                  <List.Item>
-                    <Text mark></Text> {item}
-                  </List.Item>
-                )}
-              />
-            </aside>
-          </div>
-        </DataLoadingWrapper>
-      </Layout.Content>
+          </DataLoadingWrapper>
+        </Layout.Content>
+      </Layout>
     </Layout>
   );
 }
