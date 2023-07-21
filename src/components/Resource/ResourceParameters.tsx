7dd64de9a5a82a03500b04b39108a3aba12a7f09
kavispires <kavispires@gmail.com>
2023-07-21 11:10:44 -0700

    Created ResourceGenerator page

---
+++ src/components/Resource/ResourceParameters.tsx
@@ -0,0 +1,83 @@
+import { Button, Divider, Form, Input, Select, Switch } from 'antd';
+
+import { LANGUAGES } from '../../utils/constants';
+
+export type Parameters = {
+  prefix: string;
+  language: string;
+  startingId: number;
+  headers: boolean;
+  transform: 'capitalize' | 'lowercase' | 'none' | 'uppercase';
+};
+
+export const INITIAL_PARAMETERS: Parameters = {
+  prefix: '',
+  language: 'pt',
+  startingId: 1,
+  headers: false,
+  transform: 'none',
+};
+
+type ResourceParametersProps = {
+  onUpdateParameters: Function;
+};
+
+export function ResourceParameters({ onUpdateParameters }: ResourceParametersProps) {
+  const [form] = Form.useForm();
+
+  const onFinish = (v: Parameters) => {
+    onUpdateParameters(v);
+  };
+
+  return (
+    <Form
+      form={form}
+      className="sider-content"
+      layout="vertical"
+      onFinish={onFinish}
+      size="small"
+      initialValues={INITIAL_PARAMETERS}
+    >
+      <Form.Item label="Prefix" name="prefix" required>
+        <Input type="text" placeholder="Prefix" />
+      </Form.Item>
+      <Form.Item label="Language" name="language">
+        <Select style={{ minWidth: '150px' }}>
+          {LANGUAGES.map((entry) => (
+            <Select.Option key={entry} value={entry}>
+              {entry}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item label="Starting Id" name="startingId">
+        <Input type="number" />
+      </Form.Item>
+      <Form.Item label="First Line Headers?" name="headers">
+        <Switch />
+      </Form.Item>
+      <Form.Item label="Transform text" name="transform">
+        <Select style={{ minWidth: '150px' }}>
+          <Select.Option key="none" value="none">
+            None
+          </Select.Option>
+          <Select.Option key="capitalize" value="capitalize">
+            Capitalize
+          </Select.Option>
+          <Select.Option key="lowercase" value="lowercase">
+            Lowercase
+          </Select.Option>
+          <Select.Option key="uppercase" value="uppercase">
+            Uppercase
+          </Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Generate
+        </Button>
+      </Form.Item>
+      <Divider />
+    </Form>
+  );
+}
--- src/pages/ResourceGenerator.tsx
+++ src/pages/ResourceGenerator.tsx
@@ -1,60 +1,95 @@
-import { Layout, Tabs } from 'antd';
-import { ResourceCards } from 'components/Resource/ResourceCards';
-import { ResourceJson } from 'components/Resource/ResourceJson';
-import { ResourceTable } from 'components/Resource/ResourceTable';
+import { Input, Layout } from 'antd';
 import { useState } from 'react';
 import { useTitle } from 'react-use';
-import { DataLoadingWrapper } from '../components/DataLoadingWrapper';
-import { ResourceSelectionFilters } from '../components/Resource/ResourceSelectionFilters';
-import { useQueryParams } from '../hooks/useQueryParams';
-import { useResourceState } from '../hooks/useResourceState';
-import { RESOURCE_NAMES } from '../utils/constants';
 import { Header } from 'components/Layout/Header';
-
-const resourceNames = Object.values(RESOURCE_NAMES);
+import { INITIAL_PARAMETERS, Parameters, ResourceParameters } from 'components/Resource/ResourceParameters';
+import { SectionTitle } from 'components/Common/SectionTitle';
+import { CopyToClipboardButton } from 'components/CopyToClipboardButton';
 
 export function ResourceGenerator() {
   useTitle('Resource Generator');
-  const [activeTab, setActiveTab] = useState('json');
+  const [parameters, setParameters] = useState<Parameters>(INITIAL_PARAMETERS);
+  const [input, setInput] = useState('');
+
+  const result = parseInput(input, parameters);
+  const stringifiedResult = JSON.stringify(result, null, 4);
 
   return (
     <Layout className="container">
-      <Header title="Resource Generator" subtitle="something" />
-      {/* <ResourceSelectionFilters
-        title={`Data for ${resourceName}-${language}`}
-        initialValues={{
-          resourceName,
-          language,
-        }}
-        resourceNames={resourceNames}
-        values={params}
-        updateState={updateResource}
-        hasResponseData={hasResponseData}
-        loading={loading}
-        error={error}
-      /> */}
+      <Header title="Resource Generator" />
 
-      <Layout.Content className="content">
-        {/* <DataLoadingWrapper loading={loading} error={error} hasResponseData={hasResponseData}>
-          <Tabs
-            defaultActiveKey={activeTab}
-            onChange={(tabKey) => setActiveTab(tabKey)}
-            className="page-content-tabs"
-          >
-            <Tabs.TabPane tab="JSON" key="json">
-              <ResourceJson response={response ?? {}} resourceName={resourceName ?? ''} />
-            </Tabs.TabPane>
+      <Layout hasSider>
+        <Layout.Sider className="sider">
+          <ResourceParameters onUpdateParameters={setParameters} />
+        </Layout.Sider>
 
-            <Tabs.TabPane tab="Cards" key="cards">
-              <ResourceCards response={response ?? {}} resourceName={resourceName ?? ''} />
-            </Tabs.TabPane>
-
-            <Tabs.TabPane tab="Table" key="table">
-              <ResourceTable response={response ?? {}} resourceName={resourceName ?? ''} />
-            </Tabs.TabPane>
-          </Tabs>
-        </DataLoadingWrapper> */}
-      </Layout.Content>
+        <Layout.Content className="content page-content page-content--5-5">
+          <div className="one-side">
+            <SectionTitle>Input</SectionTitle>
+            <Input.TextArea
+              name="output"
+              id=""
+              cols={15}
+              rows={20}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
+          <div className="other-side">
+            <SectionTitle>
+              Output <CopyToClipboardButton content={stringifiedResult} />
+            </SectionTitle>
+            <Input.TextArea name="output" id="" cols={15} rows={20} readOnly value={stringifiedResult} />
+          </div>
+        </Layout.Content>
+      </Layout>
     </Layout>
   );
+}
+
+function parseInput(input: string, parameters: Parameters): Record<string, GenericCard> {
+  const { prefix, language, startingId, headers: hasHeaders, transform } = parameters;
+
+  if (!prefix) {
+    return {};
+  }
+
+  const lines = input.split('\n');
+  const headers = lines[0].split('\t');
+
+  const data: Record<string, GenericCard> = {};
+
+  for (let i = hasHeaders ? 1 : 0; i < lines.length; i++) {
+    const currentLine = lines[i].split('\t');
+
+    const id = `${prefix}-${startingId + (hasHeaders ? -1 : 0) + i}-${language}`;
+
+    const card: GenericCard = {
+      id,
+    };
+
+    if (hasHeaders) {
+      for (let j = 0; j < headers.length; j++) {
+        card[headers[j]] = transformText(currentLine[j]?.trim(), transform);
+      }
+    } else {
+      card.text = transformText(currentLine[0]?.trim(), transform);
+    }
+
+    data[id] = card;
+  }
+
+  return data;
+}
+
+function transformText(text: string, transform: Parameters['transform']): string {
+  if (transform === 'capitalize') {
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  }
+  if (transform === 'lowercase') {
+    return text.toLowerCase();
+  }
+  if (transform === 'uppercase') {
+    return text.toUpperCase();
+  }
+  return text;
 }
