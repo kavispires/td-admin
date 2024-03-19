896c639415f9e81ad1ff35c497ebd0ec6c10cac2
kavispires <kavispires@gmail.com>
2024-03-18 23:06:45 -0700

    Added Items download button

--- src/components/Items/ItemListingFilters.tsx
+++ src/components/Items/ItemListingFilters.tsx
@@ -1,5 +1,6 @@
-import { Button, Divider } from 'antd';
+import { Button, Divider, Flex } from 'antd';
 import { FilterSwitch } from 'components/Common';
+import { DownloadButton } from 'components/Common/DownloadButton';
 import { SiderContent } from 'components/Layout';
 import { useItemsContext } from 'context/ItemsContext';
 
@@ -8,13 +9,17 @@ type ItemListingFiltersProps = {
   toggleSearch: () => void;
 };
 export function ItemListingFilters({ showSearch, toggleSearch }: ItemListingFiltersProps) {
-  const { isDirty, save } = useItemsContext();
+  const { isDirty, save, items } = useItemsContext();
   return (
     <SiderContent>
-      <Button block danger type="primary" disabled={!isDirty} onClick={save}>
-        Save
-      </Button>
+      <Flex vertical gap={6}>
+        <Button block danger type="primary" disabled={!isDirty} onClick={save} size="large">
+          Save
+        </Button>
+        <DownloadButton data={items} fileName="items.json" disabled={isDirty} block />
+      </Flex>
       <Divider />
+
       <FilterSwitch label="Show Search" value={showSearch} onChange={toggleSearch} />
     </SiderContent>
   );
