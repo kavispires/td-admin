191b8721261cb3c9a27c6d96a6277f9c811f1ec9
kavispires <kavispires@gmail.com>
2024-10-21 18:22:07 -0700

    Items: Added find in listing button in search

--- src/components/Items/ItemSearch.tsx
+++ src/components/Items/ItemSearch.tsx
@@ -1,14 +1,26 @@
-import { Divider, Space, Typography } from 'antd';
+import { Button, Divider, Space, Typography } from 'antd';
 import { Fragment, useState } from 'react';
 import { Item as ItemT } from 'types';
 
 import { ItemCard } from './ItemCard';
 import { useItemsContext } from 'context/ItemsContext';
 import { ItemsTypeahead } from './ItemsTypeahead';
+import { EnvironmentOutlined } from '@ant-design/icons';
+import { useQueryParams } from 'hooks/useQueryParams';
 
 export function ItemSearch() {
   const { items, isLoading, isSaving } = useItemsContext();
   const [activeItem, setActiveItem] = useState<ItemT | null>(null);
+  const { queryParams, addParam } = useQueryParams();
+
+  const onFindInListing = () => {
+    if (activeItem) {
+      const pageSize = Number(queryParams.get('pageSize') ?? 64);
+      const idNum = Number(activeItem.id);
+      const page = Math.floor(idNum / pageSize) + 1;
+      addParam('page', page.toString());
+    }
+  };
 
   return (
     <Fragment key={`item-search-${isLoading}`}>
@@ -25,6 +37,9 @@ export function ItemSearch() {
       {Boolean(activeItem) && (
         <Space direction="vertical" className="my-4" key={activeItem?.id}>
           <ItemCard item={activeItem!} />
+          <Button onClick={onFindInListing} block icon={<EnvironmentOutlined />}>
+            Find in listing
+          </Button>
         </Space>
       )}
       <Divider />
