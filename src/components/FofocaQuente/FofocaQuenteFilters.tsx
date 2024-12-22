934f621290afe897945da8cdc1e3ead1e11d970b
kavispires <kavispires@gmail.com>
2024-12-22 11:15:23 -0800

    Removed unused variables and imports

--- src/components/FofocaQuente/FofocaQuenteFilters.tsx
+++ src/components/FofocaQuente/FofocaQuenteFilters.tsx
@@ -1,23 +1,12 @@
-import { Divider, Flex, Typography } from 'antd';
-import { FilterSelect, FilterSwitch } from 'components/Common';
-import { DownloadButton } from 'components/Common/DownloadButton';
 import { SiderContent } from 'components/Layout';
-import { useItemsContext } from 'context/ItemsContext';
-import { capitalize, cloneDeep, orderBy } from 'lodash';
-import { useEffect, useMemo } from 'react';
-import { Item, TeenageStudent } from 'types';
-import { sortJsonKeys } from 'utils';
-import { useQueryParams } from 'hooks/useQueryParams';
-import { SaveButton } from 'components/Common/SaveButton';
-import { OpenAIOutlined } from '@ant-design/icons';
+import { TeenageStudent } from 'types';
 import { useTDResource } from 'hooks/useTDResource';
-import { buildDataFilters, DataFilters } from 'components/Common/DataFilters';
-import Sider from 'antd/es/layout/Sider';
+import { DataFilters } from 'components/Common/DataFilters';
 
 export type FofocaQuenteFiltersProps = ReturnType<typeof useTDResource<TeenageStudent>>;
 
 export function FofocaQuenteFilters({ data }: FofocaQuenteFiltersProps) {
-  const dataFilters = useMemo(() => buildDataFilters(data), [data]);
+
 
   return (
     <SiderContent>
