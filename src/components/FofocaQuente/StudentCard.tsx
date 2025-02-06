c37109d9e78c14756a149b48b25efa7e93f93b2a
kavispires <kavispires@gmail.com>
2025-02-05 22:35:35 -0800

    [fofoca-quente] Added colors to Social Group tag

--- src/components/FofocaQuente/StudentCard.tsx
+++ src/components/FofocaQuente/StudentCard.tsx
@@ -6,16 +6,18 @@ import type { TeenageStudent } from 'types';
 
 export function StudentCard({ student }: { student: TeenageStudent }) {
   return (
-    <Card hoverable style-={{ width: 240, maxWidth: 240 }} cover={<ImageCard id={student.id} width={240} />}>
+    <Card
+      hoverable
+      style-={{ width: 230, maxWidth: 230 }}
+      cover={<ImageCard id={student.id} width={230} preview={false} />}
+    >
       <Card.Meta
         avatar={getGenderIcon(student)}
         title={student.name.en}
         description={
           <Flex vertical>
             <Flex>{student.title.en}</Flex>
-            <Flex>
-              <Tag style={{ textTransform: 'uppercase' }}>{student.socialGroup}</Tag>
-            </Flex>
+            <Flex>{getSocialGroup(student.socialGroup)}</Flex>
             <Flex>
               {student.age} | {student.ethnicity}
             </Flex>
@@ -42,5 +44,151 @@ const getGenderIcon = (student: TeenageStudent) => {
       return <WomanOutlined />;
     default:
       return <QuestionOutlined />;
+  }
+};
+
+const getSocialGroup = (socialGroup: TeenageStudent['socialGroup']) => {
+  console.log(socialGroup);
+  switch (socialGroup) {
+    case 'Arts':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#fff30a',
+            color: '#746f04',
+            borderColor: '#746f04',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Immigrants':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#532b23',
+            color: '#c4867a',
+            borderColor: '#c4867a',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Jet Set':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#f914e4',
+            color: '#f1b1eb',
+            borderColor: '#f1b1eb',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Jocks':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#fa2e45',
+            color: '#ffd9dd',
+            borderColor: '#ffd9dd',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Leaders':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#015aca',
+            color: '#7eb4f8',
+            borderColor: '#7eb4f8',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Nerds':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#01efb7',
+            color: '#064939',
+            borderColor: '#064939',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Outcasts':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#ff9743',
+            color: '#853d02',
+            borderColor: '#853d02',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Special Needs':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#32c91e',
+            color: '#125908',
+            borderColor: '#125908',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    case 'Troublemakers':
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#7746c8',
+            color: '#cdb1fa',
+            borderColor: '#cdb1fa',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
+    default:
+      return (
+        <Tag
+          style={{
+            textTransform: 'uppercase',
+            width: '100%',
+            background: '#fff30a',
+            color: '#746f04',
+            borderColor: '#746f04',
+          }}
+        >
+          {socialGroup}
+        </Tag>
+      );
   }
 };
