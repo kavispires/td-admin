809821fd538200f6a9bd34d6d504684396eb9e81
kavispires <kavispires@gmail.com>
2025-04-12 09:37:47 -0700

    [contenders] Log deck data

--- src/components/Contenders/ContendersContent.tsx
+++ src/components/Contenders/ContendersContent.tsx
@@ -5,6 +5,8 @@ import { useFilterDataByDataFilters } from 'components/Common/DataFilters';
 import { PaginationWrapper } from 'components/Common/PaginationWrapper';
 import { useGridPagination } from 'hooks/useGridPagination';
 import type { useResourceFirebaseData } from 'hooks/useResourceFirebaseData';
+import { cloneDeep } from 'lodash';
+import { useEffect } from 'react';
 import { ContenderEditCard } from './ContenderEditCard';
 
 export type ContendersContentProps = ReturnType<typeof useResourceFirebaseData<ContenderCard>>;
@@ -17,6 +19,9 @@ export function ContendersContent({ data, addEntryToUpdate }: ContendersContentP
     resetter: '',
     defaultPageSize: 32,
   });
+
+  useEffect(() => parsedData(data), [data]);
+
   return (
     <>
       <>
@@ -35,3 +40,70 @@ export function ContendersContent({ data, addEntryToUpdate }: ContendersContentP
     </>
   );
 }
+
+const DECK_PRIORITY = ['random', 'cartoon', 'comics', 'pop-culture', 'movies', 'television'];
+
+const parsedData = (data: Dictionary<ContenderCard>) => {
+  const result = Object.values(cloneDeep(data)).reduce(
+    (acc: any, entry) => {
+      if (entry.decks.includes('base')) {
+        acc.base.push(entry);
+        entry.decks = ['base'];
+        return acc;
+      }
+      if (entry.exclusivity === 'pt') {
+        return acc;
+      }
+
+      const decksWithoutBase = entry.decks.filter((deck) => deck !== 'base');
+      if (decksWithoutBase.length > 1) {
+        console.log('Multiple decks', entry.name.en, entry.decks);
+      }
+
+      const deck = decksWithoutBase.find((deck) => DECK_PRIORITY.includes(deck)) || decksWithoutBase[0];
+
+      if (!acc[deck]) {
+        acc[deck] = [];
+      }
+      entry.decks = [deck];
+      acc[deck].push(entry);
+
+      return acc;
+    },
+    {
+      base: [],
+    },
+  );
+
+  Object.keys(result).forEach((key) => {
+    console.log(key, result[key].length);
+    // console.log(result[key].map((e: any) => `${e.name.en} (${e.id})`));
+    console.log(
+      result[key].map((e: any) => {
+        return {
+          id: e.id,
+          name: e.name.en,
+          description: e.description?.en ?? '',
+          deck: e.decks[0],
+        };
+      }),
+    );
+    console.log('-----------------');
+  });
+
+  const res: any[] = [];
+  Object.keys(result).forEach((key) => {
+    res.push(
+      ...result[key].map((e: any) => {
+        return {
+          id: e.id,
+          name: e.name.en,
+          description: e.description?.en ?? '',
+          deck: e.decks[0],
+        };
+      }),
+    );
+  });
+
+  console.log(res);
+};
