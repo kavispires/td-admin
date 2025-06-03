92a2ce3fe45c1fab71967612db494c5dc7e18db5
kavispires <kavispires@gmail.com>
2025-06-02 22:54:44 -0700

    Updates

--- assets/DoubleRightOutlined-Co0AeBz_.js
+++
@@ -1,1 +0,0 @@
-import{r as e,a as o,_ as n}from"./index-T4G7ifK2.js";import{D as u,a as i}from"./Pagination-xHbukPz3.js";var l=function(t,r){return e.createElement(o,n({},t,{ref:r,icon:u}))},s=e.forwardRef(l),f=function(t,r){return e.createElement(o,n({},t,{ref:r,icon:i}))},R=e.forwardRef(f);export{s as R,R as a};
