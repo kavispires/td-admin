500544b270a661c14b827d24b001154897d948e6
kavispires <kavispires@gmail.com>
2025-08-16 15:47:44 -0700

    Updates

---
+++ assets/CopyIdsButton-BbmDGqzC.js
@@ -0,0 +1,1 @@
+import{j as o,B as r}from"./index-IdPNWoIA.js";import{u as t}from"./useCopyToClipboardFunction-DWJ7c-lt.js";import{S as n}from"./index-CHVKldSs.js";function c({ids:i}){const s=t();return o.jsx(n,{direction:"vertical",size:"small",children:o.jsx(r,{onClick:()=>s(JSON.stringify(i)),size:"small",children:"Copy Ids"})})}export{c as C};
---
+++ assets/FullScreenModal-D4J4BL2m.js
@@ -0,0 +1,1 @@
+import{j as e,B as a,Q as l}from"./index-IdPNWoIA.js";import{c as t}from"./useBaseUrl-DjlPP39F.js";import{C as c}from"./index-Dxa4tw87.js";function x({open:r,onClose:s,...o}){return e.jsx(c,{bordered:!1,className:t("full-screen-modal",!r&&"full-screen-modal--hidden"),extra:e.jsx(a,{icon:e.jsx(l,{}),onClick:s,shape:"circle",type:"text"}),...o})}export{x as F};
