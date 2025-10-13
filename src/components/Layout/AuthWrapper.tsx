eff18ccd41542502616eeda986f1461ed45c8435
kavispires <kavispires@gmail.com>
2025-10-13 10:32:54 -0700

    Added auth modal

--- src/components/Layout/AuthWrapper.tsx
+++ src/components/Layout/AuthWrapper.tsx
@@ -1,8 +1,8 @@
-import { Alert, App, Spin } from 'antd';
+import { Alert, App, Button, Flex, Form, Input, Modal, Spin } from 'antd';
 import type { User } from 'firebase/auth';
 import { type ReactNode, useState } from 'react';
-import { useEffectOnce } from 'react-use';
-import { auth } from 'services/firebase';
+import { useEffectOnce, useKeyPressEvent } from 'react-use';
+import { auth, signIn } from 'services/firebase';
 
 type AuthWrapperProps = {
   children: ReactNode;
@@ -12,6 +12,7 @@ export function AuthWrapper({ children }: AuthWrapperProps) {
   const [authenticatedUser, setAuthenticatedUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const { message } = App.useApp();
+
   useEffectOnce(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -42,9 +43,86 @@ export function AuthWrapper({ children }: AuthWrapperProps) {
           showIcon
           type="error"
         />
+        <LoginModal />
       </div>
     );
   }
 
   return <>{children}</>;
+}
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 0, span: 8 },
+};
+
+function LoginModal() {
+  const [open, setOpen] = useState(0);
+  useKeyPressEvent('Escape', () => setOpen((v) => v + 1));
+
+  const [error, setError] = useState<string | object | null>(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onValuesChange = (data: PlainObject) => {
+    if (data.email) {
+      setEmail(data.email.trim());
+    }
+    if (data.password) {
+      setPassword(data.password.trim());
+    }
+  };
+
+  const onHandleSubmit = async () => {
+    setError('');
+    try {
+      const response = await signIn(email, password);
+      if (response?.user?.uid) {
+        console.log('Login successful');
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError('An unknown error occurred');
+      }
+    }
+  };
+
+  return (
+    <Modal footer={null} open={open > 3} title="Login">
+      {Boolean(error) && (
+        <Alert
+          className="login__error-alert"
+          description={JSON.stringify(error)}
+          message="Error"
+          showIcon
+          type="error"
+        />
+      )}
+      <Form
+        {...layout}
+        autoComplete="off"
+        className="login__form"
+        layout="horizontal"
+        name="sign-in"
+        onValuesChange={onValuesChange}
+      >
+        <Form.Item {...tailLayout} className="login__form-item" label="E-mail" name="email">
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item {...tailLayout} className="login__form-item" label="Password" name="password">
+          <Input type="password" />
+        </Form.Item>
+        <Flex justify="flex-end">
+          <Button onClick={onHandleSubmit} type="primary">
+            Login
+          </Button>
+        </Flex>
+      </Form>
+    </Modal>
+  );
 }
